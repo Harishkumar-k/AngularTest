f5ca17342404191deac9c5d715b74153aa03668c
Harish Kumar Kesavan (uif32855) <harish.kesavan@continental-corporation.com>
2023-07-08 10:13:38 +0530

    firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  firebaseConfig = {
+        apiKey: "AIzaSyBQ3YtXbqMNcRFRHnOgXEmCCh2HK2Pcz28",
+        authDomain: "mywebapp-a.firebaseapp.com",
+        projectId: "mywebapp-a",
+        storageBucket: "mywebapp-a.appspot.com",
+        messagingSenderId: "191125709639",
+        appId: "1:191125709639:web:fb556f37424d314f60a262",
+        measurementId: "G-TK2KMWXQQB"
+        };
   title = 'AngularTest';
+
+  constructor(){
+    let app = initializeApp(this.firebaseConfig);
+    let analytics = getAnalytics(app);
+  }
 }
